41022d66746d90c0093b04bf02a5db9d5a5f309f
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-21 21:12:08 -0400

    Agrengando ViewModel al proyecto.

---
+++ app/src/main/java/com/example/apiwexchanges/model/ExchangeViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.exampleexchanges.model
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.exampleexchanges.data.ListStateExchange
+import com.example.exampleexchanges.data.remote.repository.ExchangeRepository
+import com.example.exampleexchanges.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class ExchangeViewModel @Inject constructor(
+     private val exchangeRepository: ExchangeRepository
+) : ViewModel() {
+
+    private var _state = mutableStateOf(ListStateExchange())
+    val state: State<ListStateExchange> = _state
+
+    init {
+        exchangeRepository.getExchanges().onEach { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    _state.value = ListStateExchange(isLoading = true)
+                }
+                is Resource.Success -> {
+                    _state.value = ListStateExchange(exchange = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = ListStateExchange(error = result.message ?: "Error desconocido")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
